7ec087456a4ae02c272c7ab6e49f80dfd416cc3d
Faizal Chan. <moefc32@gmail.com>
2025-08-28 09:20:36 +0700

    use generic user agent

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,7 @@
 async function apiFetch(url, options = {}) {
 	const defaultHeaders = {
-		'User-Agent': 'Mfc Site Worker',
-		'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; Win11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36',
+        'Accept': 'application/json',
 	};
 
 	options.headers = { ...defaultHeaders, ...(options.headers || {}) };
